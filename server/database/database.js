792ca5a11f35b33686f70216fb39d8f7fd5a54fe
Menno <mennodevries1@msn.com>
2020-02-06 21:43:19 +0100

    Added mongoose and local db

---
+++ server/database/database.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
+const server = 'localhost'
+const database = 'test'
+const uristring = process.env.MONGODB_URI || `mongodb://${server}/${database}`
+
+class Database {
+  constructor() {
+    this.connect()
+  }
+
+  connect() {
+    mongoose
+      .connect(uristring, { useNewUrlParser: true })
+      .then(() => {
+        // mongoose.connection.db.dropDatabase();
+        console.log('Database connection successful: ' + uristring)
+      })
+      .catch(err => {
+        console.error('Database connection error: ' + uristring)
+      })
+  }
+}
+
+module.exports = new Database()
