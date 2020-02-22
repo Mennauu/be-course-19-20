e7ea3d1f4178de7f56291fdd6862fa87f0fe16a7
Menno <mennodevries1@msn.com>
2020-02-22 12:15:55 +0100

    Automatic file revisioning and replacing (on save)

--- server/middleware/revisioning/revision.js
+++ server/middleware/revisioning/revision.js
@@ -1,39 +1,66 @@
 const gulp = require('gulp')
 const rev = require('gulp-rev')
 const revReplace = require('gulp-rev-replace')
-const revDel = require('rev-del')
 const revDelOrig = require('gulp-rev-delete-original')
 
-gulp.task('revision', () => {
-  const srcFiles = ['../../../build/assets/css/*.css', '../../../build/assets/js/*.js']
-  const buildFolder = '../../../build/assets'
+gulp.task('revision-js', () => {
+  const srcJS = ['../../../build/assets/js/*.js']
+  const buildJsFolder = '../../../build/assets/js'
+  const buildManifestFolder = '../../../build/assets'
 
   return gulp
-    .src(srcFiles, { base: buildFolder })
+    .src(srcJS)
     .pipe(rev())
     .pipe(revDelOrig())
-    .pipe(gulp.dest(buildFolder))
-    .pipe(rev.manifest())
-    .pipe(revDel({ dist: buildFolder }))
-    .pipe(gulp.dest(buildFolder))
+    .pipe(gulp.dest(buildJsFolder))
+    .pipe(
+      rev.manifest(`${buildManifestFolder}/rev-manifest.json`, {
+        base: buildManifestFolder,
+        merge: true,
+      }),
+    )
+    .pipe(gulp.dest(buildManifestFolder))
 })
 
-gulp.task('replace', ['revision'], () => {
+gulp.task('revision-css', () => {
+  const srcCSS = ['../../../build/assets/css/*.css']
+  const buildCssFolder = '../../../build/assets/css'
+  const buildManifestFolder = '../../../build/assets'
+
+  return gulp
+    .src(srcCSS)
+    .pipe(rev())
+    .pipe(revDelOrig())
+    .pipe(gulp.dest(buildCssFolder))
+    .pipe(
+      rev.manifest(`${buildManifestFolder}/rev-manifest.json`, {
+        base: buildManifestFolder,
+        merge: true,
+      }),
+    )
+    .pipe(gulp.dest(buildManifestFolder))
+})
+
+gulp.task('replace-js', ['revision-js'], () => {
   const buildFolder = '../../../build/assets'
-  const layoutFile = '../../views/layouts/default.html'
+  const revisionFile = '../../views/revisions/default.html'
   const layoutFolder = '../../views/layouts'
   const manifest = gulp.src(`${buildFolder}/rev-manifest.json`)
 
   return gulp
-    .src(layoutFile)
+    .src(revisionFile)
     .pipe(revReplace({ manifest: manifest }))
     .pipe(gulp.dest(layoutFolder))
 })
 
-// gulp.task('clean', ['revision'], () => {
-//   const buildFolder = '../../../build/assets'
+gulp.task('replace-css', ['revision-css'], () => {
+  const buildFolder = '../../../build/assets'
+  const revisionFile = '../../views/revisions/default.html'
+  const layoutFolder = '../../views/layouts'
+  const manifest = gulp.src(`${buildFolder}/rev-manifest.json`)
 
-//   return gulp
-//     .src([`${buildFolder}/**/*`], { read: false })
-//     .pipe(revDistClean(`${buildFolder}/rev-manifest.json`))
-// })
+  return gulp
+    .src(revisionFile)
+    .pipe(revReplace({ manifest: manifest }))
+    .pipe(gulp.dest(layoutFolder))
+})
